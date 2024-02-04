b3ab5dad74c2693983fd542552307047c506d961
smillanpardo <santiago.millan@nivelics.co>
2024-02-04 14:31:50 -0500

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle('ComAnime-Back')
+    .setDescription('Documentación ComAnime')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  await app.listen(3000).then(() => {
+    console.log('Server running on port 3000');
+  });
 }
 bootstrap();
