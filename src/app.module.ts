38a61840b5968dbc8b74de9a4e38774843b2fc11
smillanpardo <santiago.millan@nivelics.co>
2024-02-04 15:25:37 -0500

    midleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,19 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AnimeService } from './modules/anime/anime.service';
 import { AnimeController } from './modules/anime/anime.controller';
 import { HttpModule } from '@nestjs/axios';
 import { AnimeModule } from './modules/anime/anime.module';
+import { AllowAnyIPMiddleware } from './middleware/auth.middleware';
 
 @Module({
   imports: [HttpModule, AnimeModule],
   controllers: [AppController, AnimeController],
   providers: [AppService, AnimeService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AllowAnyIPMiddleware).forRoutes('*');
+}
+}
