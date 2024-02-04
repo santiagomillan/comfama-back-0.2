9e11ae332209f29da5914ab09816dc81d25745b7
smillanpardo <santiago.millan@nivelics.co>
2024-02-04 17:59:24 -0500

    midleware

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,20 +1,34 @@
+// import { Injectable, NestMiddleware } from '@nestjs/common';
+// import { Request, Response, NextFunction } from 'express';
+
+// @Injectable()
+// export class AllowAnyIPMiddleware implements NestMiddleware {
+//   use(req: Request, res: Response, next: NextFunction) {
+//     // Establece los encabezados CORS para permitir cualquier origen
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header(
+//       'Access-Control-Allow-Headers',
+//       'Origin, X-Requested-With, Content-Type, Accept',
+//     );
+//     res.header(
+//       'Access-Control-Allow-Methods',
+//       'GET, POST, PUT, DELETE, OPTIONS',
+//     );
+//     // Continúa con la solicitud
+//     next();
+//   }
+// }
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class AllowAnyIPMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    // Establece los encabezados CORS para permitir cualquier origen
     res.header('Access-Control-Allow-Origin', '*');
     res.header(
       'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept',
     );
-    res.header(
-      'Access-Control-Allow-Methods',
-      'GET, POST, PUT, DELETE, OPTIONS',
-    );
-    // Continúa con la solicitud
     next();
   }
 }
